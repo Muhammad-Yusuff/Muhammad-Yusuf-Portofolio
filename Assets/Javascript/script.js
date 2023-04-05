55f64b57fa51c96ff895f9ce3216310e1f740590
Muhammad-Yusuff <my349170@gmail.com>
2023-04-05 21:01:46 +0700

    Edit design

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,16 +1,46 @@
-const burger = document.querySelector('.burger')
-const sidebar = document.querySelector('.sidebar')
-const bgSidebar = document.querySelector('.bg-sidebar')
 
-burger.addEventListener('click', function() {
-  this.classList.toggle('change')
-  sidebar.classList.toggle('change')
-  bgSidebar.classList.toggle('change')
-})
 
-bgSidebar.addEventListener('click', function() {
-  this.classList.remove('change')
-  sidebar.classList.remove('change')
-  burger.classList.remove('change')
-})
-
+/*porto1 firterabel galeri*/
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("column");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+      w3RemoveClass(x[i], "show");
+      if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+  }
+  
+  function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {
+        element.className += " " + arr2[i];
+      }
+    }
+  }
+  
+  function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+  
+  var btnContainer = document.getElementById("myBtnContainer");
+  var btns = btnContainer.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(){
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }

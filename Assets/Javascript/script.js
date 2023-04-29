1c1993ba7fb811aa08945004a3c2991636d96672
Muhammad-Yusuff <my349170@gmail.com>
2023-04-29 20:06:19 +0700

    icon loader

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,4 +1,17 @@
-
+/*animasi Loading*/
+document.onreadystatechange = function() { 
+  if (document.readyState !== "complete") { 
+      document.querySelector( 
+      "body").style.visibility = "hidden"; 
+      document.querySelector( 
+      "#loader").style.visibility = "visible"; 
+  } else { 
+      document.querySelector( 
+      "#loader").style.display = "none"; 
+      document.querySelector( 
+      "body").style.visibility = "visible"; 
+  } 
+}; 
 
 /*porto1 firterabel galeri*/
 filterSelection("all")
